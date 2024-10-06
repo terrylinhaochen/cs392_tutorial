9eb2c28515b375a7c7627329fb5eb4905fbc8b98
TerryChenRDFZ <terrylinhaochen@163.com>
2024-10-05 23:48:33 -0500

    work in progress for list of courses

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,36 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const schedule = {
+  "title": "CS Courses for 2018-2019",
+  "courses": {
+    "F101" : {
+      "term": "Fall",
+      "number": "101",
+      "meets" : "MWF 11:00-11:50",
+      "title" : "Computer Science: Concepts, Philosophy, and Connections"
+    },
+    "F110" : {
+      "term": "Fall",
+      "number": "110",
+      "meets" : "MWF 10:00-10:50",
+      "title" : "Intro Programming for non-majors"
+    },
+    "S313" : {
+      "term": "Spring",
+      "number": "313",
+      "meets" : "TuTh 15:30-16:50",
+      "title" : "Tangible Interaction Design and Learning"
+    },
+    "S314" : {
+      "term": "Spring",
+      "number": "314",
+      "meets" : "TuTh 9:30-10:50",
+      "title" : "Tech & Human Interaction"
+    }
+  }
+};
+
 const App = () => {
   const today = new Date();
   const day = today.toLocaleString([], {weekday: 'long'});

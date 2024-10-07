77083d0b402720d00f9adaa85414596723167698
TerryChenRDFZ <terrylinhaochen@163.com>
2024-10-07 11:36:50 -0500

    updated deployment to firebase

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
 
-const CourseList = ({ courses }) => (
-  <div>
-    <h2>Course List</h2>
-    <table>
-      <thead>
-        <tr>
-          <th>Term</th>
-          <th>Number</th>
-          <th>Title</th>
-          <th>Meeting Times</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Object.entries(courses).map(([id, course]) => (
-          <tr key={id}>
-            <td>{course.term}</td>
-            <td>{course.number}</td>
-            <td>{course.title}</td>
-            <td>{course.meets}</td>
-          </tr>
+const CourseCard = ({ course }) => (
+    <div className="card m-1 p-2" style={{width: '18rem'}}>
+      <div className="card-body">
+        <h5 className="card-title">{course.term} {course.number}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{course.title}</h6>
+        <p className="card-text">{course.meets}</p>
+      </div>
+    </div>
+  );
+  
+  const CourseList = ({ courses }) => (
+    <div className="course-list">
+      <div className="row">
+        {Object.values(courses).map((course, index) => (
+          <div key={index} className="col-lg-3 col-md-4 col-sm-6 mb-3">
+            <CourseCard course={course} />
+          </div>
         ))}
-      </tbody>
-    </table>
-  </div>
-);
+      </div>
+    </div>
+  );  
 
 export default CourseList;
